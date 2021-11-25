91b1451431fdf021c39960fb60b22705606772b7
lujeremy <jeremylu04@gmail.com>
2021-11-25 15:46:44 -0500

    Update gitignore, Add cmake

--- draw.c
+++ draw.c
@@ -1,65 +1,38 @@
-// C program to demonstrate
-// drawing a circle using
-// OpenGL
-#include<stdio.h>
-#include<GL/glut.h>
-#include<math.h>
-#define pi 3.142857
- 
-// function to initialize
-void myInit (void)
+#include <GLFW/glfw3.h>
+
+int main(void)
 {
-    // making background color black as first
-    // 3 arguments all are 0.0
-    glClearColor(0.0, 0.0, 0.0, 1.0);
-     
-    // making picture color green (in RGB mode), as middle argument is 1.0
-    glColor3f(0.0, 1.0, 0.0);
-     
-    // breadth of picture boundary is 1 pixel
-    glPointSize(1.0);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-     
-    // setting window dimension in X- and Y- direction
-    gluOrtho2D(-780, 780, -420, 420);
-}
- 
-void display (void)
-{
-    glClear(GL_COLOR_BUFFER_BIT);
-    glBegin(GL_POINTS);
-    float x, y, i;
-     
-    // iterate y up to 2*pi, i.e., 360 degree
-    // with small increment in angle as
-    // glVertex2i just draws a point on specified co-ordinate
-    for ( i = 0; i < (2 * pi); i += 0.001)
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "GLFW CMake starter", NULL, NULL);
+    if (!window)
     {
-        // let 200 is radius of circle and as,
-        // circle is defined as x=r*cos(i) and y=r*sin(i)
-        x = 200 * cos(i);
-        y = 200 * sin(i);
-         
-        glVertex2i(x, y);
+        glfwTerminate();
+        return -1;
     }
-    glEnd();
-    glFlush();
-}
- 
-int main (int argc, char** argv)
-{
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-     
-    // giving window size in X- and Y- direction
-    glutInitWindowSize(1366, 768);
-    glutInitWindowPosition(0, 0);
-     
-    // Giving name to window
-    glutCreateWindow("Circle Drawing");
-    myInit();
-     
-    glutDisplayFunc(display);
-    glutMainLoop();
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+    glClearColor( 0.4f, 0.3f, 0.4f, 0.0f );
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
 }
