66f66ee139b2230d4b58ed5b37763db9fbcfba86
melodyhsu47 <36453835+melodyhsu47@users.noreply.github.com>
2021-12-13 10:38:19 -0500

    Add files via upload

---
+++ draw.c
@@ -0,0 +1,54 @@
+#include <GLFW/glfw3.h>
+#include <OpenGL/gl.h>
+#include <GLUT/glut.h>
+
+void draw(int c)
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return;
+
+#ifdef __APPLE__
+    glfwWindowHint (GLFW_CONTEXT_VERSION_MAJOR,3);
+    glfwWindowHint (GLFW_CONTEXT_VERSION_MINOR,2);
+    glfwWindowHint (GLFW_OPENGL_FORWARD_COMPAT,GL_TRUE);
+    glfwWindowHint (GLFW_OPENGL_PROFILE,GLFW_OPENGL_CORE_PROFILE);
+#endif
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "GLFW CMake starter", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+    glClearColor( 0.4f, 0.3f, 0.4f, 0.0f );
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return;
+}
+
+#ifdef BUILD_TEST
+int main(int argc, char *argv[])
+{
+  draw(1);
+}
+#endif
