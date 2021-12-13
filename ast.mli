5cee5c6e64789d7824e5ed7fee1e48ccb05914bd
melodyhsu47 <36453835+melodyhsu47@users.noreply.github.com>
2021-12-13 10:36:19 -0500

    Delete ast.mli

--- ast.mli
+++
@@ -1,9 +0,0 @@
-type operator = Add | Sub | Mul | Div
-
-type expr =
-    Binop of expr * operator * expr
-  | Lit of int
-  | Asg of string * expr
-  | Seq of expr * expr
-  | Var of string
-  | If of expr * expr * expr
