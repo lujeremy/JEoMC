b12f850f05e87dea9b3e35bfa4fd1b1efadc1c55
melodyhsu47 <36453835+melodyhsu47@users.noreply.github.com>
2021-12-19 00:09:02 -0500

    add multiple triangle draw

--- draw2.c
+++ draw2.c
@@ -5,6 +5,7 @@
 #include <GLFW/glfw3.h>
 #include "stb_image_write.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /* MacOS imports */
 #if __has_include(<OpenGL/gl.h>)
@@ -22,6 +23,8 @@
 #include <GL/glut.h>
 #endif
 
+
+
 void saveImage(char *filepath, GLFWwindow *w){
     int width, height;
     glfwGetFramebufferSize(w,&width,&height);
@@ -37,7 +40,7 @@ void saveImage(char *filepath, GLFWwindow *w){
     stbi_write_png(filepath,width,height,nrChannels,buffer,stride);
 }
 
-void draw2(int c)
+void draw2(int iters)
 {
     GLFWwindow* window;
 
@@ -62,73 +65,84 @@ void draw2(int c)
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
-    glClearColor( 0.1f, 0.1f, 0.1f, 0.0f );
+    glClearColor( 1.0f, 1.0f, 1.0f, 1.0f );
 
     glewExperimental = GL_TRUE;
     glewInit();
 
-    //uncomment below to draw individual points
-    /*
-    float points[] = {
-	0.0f, 0.5f, 0.0f,
-	0.5f, -0.5f, 0.0f,
-	-0.5f, -0.5f, 0.0f
-    };
-    */
+    //generate points
+
+    double yvals[iters+1];
+
+    int numpts = 0;
+    for (int i=iters+1; i>0; i--){
+        numpts += i;
+    }
+    //static double *triangles;
+    double triangles[3*numpts];
+
+    for (int i=0; i<iters+1; i++){
+        yvals[i] = -1.0 + 2*(i/(double) iters);
+    }
+    //calculate increment size - half triangle base length
+    double base = 1.0/iters;
+    int init = iters;
+    int pos = 0;
+    for (int a=0; a<=iters; a++){
+        double y = yvals[a];
+        double start = -1.0 + a*base;
+        for (int b=0; b<=init; b++){
+            double x = start + b*(2*base);
+            //printf("(x,y):(%0.2f,%0.2f) ",x,y);
+            triangles[pos++] = x;
+            triangles[pos++] = y;
+            triangles[pos++] = 0.0;
+        }
+        init--;
+        //printf("\n");
+    }
+
+    int numtri = numpts - (iters+1);
     
-    //uncomment below to draw lines
+    //make array that will draw triangles
+    double tripoints[3*3*numtri];
+    //3 coordinates per point, 3 points per triangle
+    int p = iters;
+    int trng_ind = 0;
+    int numtri_ind = 0;
+    int which_triangle = 0;
     
-    float points[] = {
-    0.0f,0.125f,0.0f,
-    0.0625f,0.0f,0.0f,
+    for (int i=0; i<iters; i++){
+    //below loop adds one row's triangle coordinates
+    	for (int j=0; j<p; j++){
+        	int trng_ind = 3*which_triangle;
+        	//add 3 elems for 2 base points, so need loop
+        	for (int k=0; k<6; k++){
+        		tripoints[numtri_ind++] = triangles[trng_ind++];
+        	}
+        	//skip (iter-1)*3 points, get tip coordinate
+        	trng_ind += (p-1)*3;
+        	for (int k=0; k<3; k++){
+        		tripoints[numtri_ind++] = triangles[trng_ind++];
+        	}
+        	which_triangle++;
+    	}
+    	which_triangle++;
+    	p--;
+    }
 
-    0.0625f,0.0f,0.0f,
-    -0.0625f,0.0f,0.0f,
-
-    -0.0625f,0.0f,0.0f,
-    0.0f,0.125f,0.0f,
-
-    0.0f,0.25f,0.0f,
-    0.125f,0.0f,0.0f,
-
-    0.125f,0.0f,0.0f,
-    -0.125f,0.0f,0.0f,
-
-    -0.125f,0.0f,0.0f,
-    0.0f,0.25f,0.0f,
-
-	0.0f,0.5f,0.0f,
-	0.25f,0.0f,0.0f,
-
-	0.25f,0.0f,0.0f,
-	-0.25f,0.0f,0.0f,
-
-	-0.25f,0.0f,0.0f,
-	0.0f,0.5f,0.0f,
-	
-	0.0f,1.0f,0.0f,
-	0.5f,0.0f,0.0f,
-
-	0.5f,0.0f,0.0f,
-	-0.5f,0.0f,0.0f,
-
-	-0.5f,0.0f,0.0f,
-	0.0f,1.0f,0.0f
-	
-    };
-    
-
+    //THIS IS WHERE YOU SELECT WHICH ARRAY TO DRAW!!! (glBufferData)
     GLuint vbo = 0;
     glGenBuffers(1,&vbo);
     glBindBuffer(GL_ARRAY_BUFFER,vbo);
-    glBufferData(GL_ARRAY_BUFFER,sizeof(points),points,GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER,sizeof(tripoints),tripoints,GL_STATIC_DRAW);
 
     GLuint vao = 0;
     glGenVertexArrays(1,&vao);
     glBindVertexArray(vao);
     glEnableVertexAttribArray(0);
     glBindBuffer(GL_ARRAY_BUFFER,vbo);
-    glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,0,NULL);
+    glVertexAttribPointer(0,3,GL_DOUBLE,GL_FALSE,0,NULL);
 
     const char *vertex_shader =
 	"#version 330\n"
@@ -170,17 +184,17 @@ void draw2(int c)
 	glBindVertexArray(vao);
 
 	//use to draw a triangle---------------
-	//glDrawArrays(GL_TRIANGLES,0,3);
+	glDrawArrays(GL_TRIANGLES,0,numtri*3);
 	//-------------------------------------
 	
 	//use to draw points------------------
 	glEnable(GL_VERTEX_PROGRAM_POINT_SIZE);
-	//glDrawArrays(GL_POINTS,0,3);
+	//glDrawArrays(GL_POINTS,0,numpts);
 	//------------------------------------
 
 	//use to draw lines--------------------
-	glPolygonMode(GL_FRONT_AND_BACK,GL_LINE);
-	glDrawArrays(GL_LINES,0,24);
+	//glPolygonMode(GL_FRONT_AND_BACK,GL_LINE);
+	//glDrawArrays(GL_LINES,0,24);
 	//-------------------------------------
 
         /* Swap front and back buffers */
@@ -201,6 +215,6 @@ void draw2(int c)
 #ifdef BUILD_TEST
 int main(int argc, char *argv[])
 {
-  draw2(1);
+  draw2(6);
 }
 #endif
