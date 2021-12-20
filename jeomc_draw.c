fc817f0882c61d6eee641ffc84751968f24faab2
melodyhsu47 <36453835+melodyhsu47@users.noreply.github.com>
2021-12-19 20:09:12 -0500

    fix typo in apple ifdef

--- jeomc_draw.c
+++ jeomc_draw.c
@@ -66,7 +66,7 @@ void jeomcInit() {
     glfwWindowHint (GLFW_CONTEXT_VERSION_MAJOR,3);
     glfwWindowHint (GLFW_CONTEXT_VERSION_MINOR,2);
     glfwWindowHint (GLFW_OPENGL_FORWARD_COMPAT,GL_TRUE);
-    glfwWindowHint (GLFW_OPEN_PROFILE,GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint (GLFW_OPENGL_PROFILE,GLFW_OPENGL_CORE_PROFILE);
 #endif
 
     /* Create a windowed mode window and its OpenGL context */
